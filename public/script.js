0feb7025e0b3ca0010c12d17f42ec91ddf9c0238
allynielsen <86061526+allynielsen@users.noreply.github.com>
2021-12-09 13:45:53 -0500

    old commit

--- public/script.js
+++ public/script.js
@@ -1,116 +1,116 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // Get all "navbar-burger" elements
-  const $navbarBurgers = Array.prototype.slice.call(
-    document.querySelectorAll(".navbar-burger"),
-    0
-  );
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('Yay HTML is connecting to script');
+  const form = document.querySelector('form');
 
-  // Check if there are any navbar burgers
-  if ($navbarBurgers.length > 0) {
-    // Add a click event on each of them
-    $navbarBurgers.forEach((el) => {
-      el.addEventListener("click", () => {
-        // Get the target from the "data-target" attribute
-        const target = el.dataset.target;
-        const $target = document.getElementById(target);
-
-        // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-        el.classList.toggle("is-active");
-        $target.classList.toggle("is-active");
-      });
-    });
-  }
+  form.addEventListener('submit', (event) => {
+    console.log('form was submitted!');
+  });
 });
+//   // Check if there are any navbar burgers
+//   if ($navbarBurgers.length > 0) {
+//     // Add a click event on each of them
+//     $navbarBurgers.forEach((el) => {
+//       el.addEventListener('click', () => {
+//         // Get the target from the "data-target" attribute
+//         const {target} = el.dataset;
+//         const $target = document.getElementById(target);
 
-var app = new (function () {
-  this.el = document.getElementById("tasks");
+//         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+//         el.classList.toggle('is-active');
+//         $target.classList.toggle('is-active');
+//       });
+//     });
+//   }
+// });
 
-  this.tasks = [];
+// const app = new (function () {
+//   this.el = document.getElementById('tasks');
 
-  this.FetchAll = function () {
-    var data = "";
+//   this.tasks = [];
 
-    if (this.tasks.length > 0) {
-      for (i = 0; i < this.tasks.length; i++) {
-        data += "<tr>";
-        data += "<td>" + (i + 1) + ". " + this.tasks[i] + "</td>";
-        data +=
-          '<td><button onclick="app.Edit(' +
-          i +
-          ')"  class="btn btn-warning">Edit</button></td>';
-        data +=
-          '<td><button onclick="app.Delete(' +
-          i +
-          ')"  class="btn btn-danger">Delete</button></td>';
-        data += "</tr>";
-      }
-    }
+//   this.FetchAll = function () {
+//     let data = '';
 
-    this.Count(this.tasks.length);
-    return (this.el.innerHTML = data);
-  };
+//     if (this.tasks.length > 0) {
+//       for (i = 0; i < this.tasks.length; i++) {
+//         data += '<tr>';
+//         data += `<td>${i + 1}. ${this.tasks[i]}</td>`;
+//         data
+//           += `<td><button onclick="app.Edit(${
+//             i
+//           })"  class="btn btn-warning">Edit</button></td>`;
+//         data
+//           += `<td><button onclick="app.Delete(${
+//             i
+//           })"  class="btn btn-danger">Delete</button></td>`;
+//         data += '</tr>';
+//       }
+//     }
 
-  this.Add = function () {
-    el = document.getElementById("add-todo");
-    // Get the value
-    var task = el.value;
+//     this.Count(this.tasks.length);
+//     return (this.el.innerHTML = data);
+//   };
 
-    if (task) {
-      // Add the new value
-      this.tasks.push(task.trim());
-      // Reset input value
-      el.value = "";
-      // Dislay the new list
-      this.FetchAll();
-    }
-  };
-  this.Edit = function (item) {
-    var el = document.getElementById("edit-todo");
-    // Display value in the field
-    el.value = this.tasks[item];
-    // Display fields
-    document.getElementById("edit-box").style.display = "block";
-    self = this;
+//   this.Add = function () {
+//     el = document.getElementById('add-todo');
+//     // Get the value
+//     const task = el.value;
 
-    document.getElementById("save-edit").onsubmit = function () {
-      // Get value
-      var task = el.value;
+//     if (task) {
+//       // Add the new value
+//       this.tasks.push(task.trim());
+//       // Reset input value
+//       el.value = '';
+//       // Dislay the new list
+//       this.FetchAll();
+//     }
+//   };
+//   this.Edit = function (item) {
+//     const el = document.getElementById('edit-todo');
+//     // Display value in the field
+//     el.value = this.tasks[item];
+//     // Display fields
+//     document.getElementById('edit-box').style.display = 'block';
+//     self = this;
 
-      if (task) {
-        // Edit value
-        self.tasks.splice(item, 1, task.trim());
-        // Display the new list
-        self.FetchAll();
-        // Hide fields
-        CloseInput();
-      }
-    };
-  };
+//     document.getElementById('save-edit').onsubmit = function () {
+//       // Get value
+//       const task = el.value;
 
-  this.Delete = function (item) {
-    // Delete the current row
-    this.tasks.splice(item, 1);
-    // Display the new list
-    this.FetchAll();
-  };
+//       if (task) {
+//         // Edit value
+//         self.tasks.splice(item, 1, task.trim());
+//         // Display the new list
+//         self.FetchAll();
+//         // Hide fields
+//         CloseInput();
+//       }
+//     };
+//   };
 
-  this.Count = function (data) {
-    var el = document.getElementById("counter");
-    var name = "Tasks";
+//   this.Delete = function (item) {
+//     // Delete the current row
+//     this.tasks.splice(item, 1);
+//     // Display the new list
+//     this.FetchAll();
+//   };
 
-    if (data) {
-      if (data == 1) {
-        name = "Task";
-      }
-      el.innerHTML = data + " " + name;
-    } else {
-      el.innerHTML = "No " + name;
-    }
-  };
-})();
+//   this.Count = function (data) {
+//     const el = document.getElementById('counter');
+//     let name = 'Tasks';
 
-app.FetchAll();
+//     if (data) {
+//       if (data == 1) {
+//         name = 'Task';
+//       }
+//       el.innerHTML = `${data} ${name}`;
+//     } else {
+//       el.innerHTML = `No ${name}`;
+//     }
+//   };
+// })();
 
-function CloseInput() {
-  document.getElementById("edit-box").style.display = "none";
-}
+// app.FetchAll();
+
+// function CloseInput() {
+//   document.getElementById('edit-box').style.display = 'none';
