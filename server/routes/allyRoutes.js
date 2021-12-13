49e7a7ab2e060181a4edad92e2605b4e1fbc5fe9
enyu0510 <jonathanchen0510@gmail.com>
2021-12-13 13:25:21 -0500

    Change folder structure

--- server/routes/allyRoutes.js
+++ server/routes/allyRoutes.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import sequelize from 'sequelize';
+import express from "express";
+import sequelize from "sequelize";
 // eslint-disable-next-line import/no-unresolved
-import extinctionSQL from '../server/controllers/allyController.js';
-import db from '../database/initializeDB.js';
+import extinctionSQL from "../controllers/allyController.js";
+import db from "../../database/initializeDB.js";
 
 const router = express.Router();
 
@@ -11,38 +11,41 @@ const router = express.Router();
 /// /////////////////////////////////
 
 router
-  .route('/extinction')
+  .route("/extinction")
   // eslint-disable-next-line consistent-return
   .get(async (req, res) => {
     try {
       // eslint-disable-next-line no-shadow
       const extinction = await db.sequelizeDB.query(extinctionSQL, {
-        type: sequelize.QueryTypes.SELECT
+        type: sequelize.QueryTypes.SELECT,
       });
-      const reply = extinction.length > 0 ? { data: extinction } : { message: 'no results found' };
-      console.log('touched /extinction with GET');
+      const reply =
+        extinction.length > 0
+          ? { data: extinction }
+          : { message: "no results found" };
+      console.log("touched /extinction with GET");
       res.json(reply);
       return reply;
     } catch (err) {
       console.error(err);
-      res.send('Server error');
+      res.send("Server error");
     }
   })
   .post(async (req, res) => {
     const extinction = await db.sequelizeDB.query(extinctionSQL, {
-      type: sequelize.QueryTypes.SELECT
+      type: sequelize.QueryTypes.SELECT,
     });
     try {
       const newExtinction = await db.extinction.create({
         extinction_id: currentId,
         cause: req.body.cause,
-        age_species_went_extinct: req.body.age_species_went_extinct
+        age_species_went_extinct: req.body.age_species_went_extinct,
       });
-      console.log('touched /extinction with POST');
+      console.log("touched /extinction with POST");
       res.json(newExtinction);
     } catch (err) {
       console.error(err);
-      res.error('Server error');
+      res.error("Server error");
     }
   })
   .put(async (req, res) => {
@@ -50,32 +53,32 @@ router
       await db.extinction.update(
         {
           cause: req.body.cause,
-          age_species_went_extinct: req.body.age_species_went_extinct
+          age_species_went_extinct: req.body.age_species_went_extinct,
         },
         {
           where: {
-            extinction_id: req.body.extinction_id
-          }
+            extinction_id: req.body.extinction_id,
+          },
         }
       );
-      console.log('touched /extinction with PUT');
-      res.send('Successfully Updated');
+      console.log("touched /extinction with PUT");
+      res.send("Successfully Updated");
     } catch (err) {
       console.error(err);
-      res.error('Server error');
+      res.error("Server error");
     }
   })
   .delete(async (req, res) => {
     try {
       await db.extinction.destroy({
         where: {
-          extinction_id: req.params.extinction_id
-        }
+          extinction_id: req.params.extinction_id,
+        },
       });
-      res.send('Successfully Deleted');
+      res.send("Successfully Deleted");
     } catch (err) {
       console.error(err);
-      res.error('Server error');
+      res.error("Server error");
     }
   });
 export default router;
--- server/routes/peterRoutes.js
+++ server/routes/peterRoutes.js
@@ -1,8 +1,8 @@
 /* eslint-disable no-console */
-import express from 'express';
-import sequelize from 'sequelize';
-import db from '../database/initializeDB.js';
-import hierarchySQL from '../server/controllers/peterController.js';
+import express from "express";
+import sequelize from "sequelize";
+import db from "../../database/initializeDB.js";
+import hierarchySQL from "../controllers/peterController.js";
 
 const router = express.Router();
 
@@ -10,20 +10,24 @@ const router = express.Router();
 /// ////Animals Database - Hierarchy - Peter Andrade ///
 /// /////////////////////////////////
 
-router.route('/hierarchy')
+router
+  .route("/hierarchy")
   // eslint-disable-next-line consistent-return
   .get(async (req, res) => {
     try {
       const hierarchy = await db.sequelizeDB.query(hierarchySQL, {
-        type: sequelize.QueryTypes.SELECT
+        type: sequelize.QueryTypes.SELECT,
       });
-      const reply = hierarchy.length > 0 ? { data: hierarchy } : { message: 'no results found' };
-      console.log('touched /hierarchy with GET');
+      const reply =
+        hierarchy.length > 0
+          ? { data: hierarchy }
+          : { message: "no results found" };
+      console.log("touched /hierarchy with GET");
       res.json(reply);
       return reply;
     } catch (err) {
       console.error(err);
-      res.send('Server error');
+      res.send("Server error");
     }
   })
 
@@ -32,39 +36,39 @@ router.route('/hierarchy')
       await db.hierarchy.update(
         {
           class: req.body.class,
-          phylum: req.body.phylum
+          phylum: req.body.phylum,
         },
         {
           where: {
-            hierarchy_id: req.body.hierarchy_id
-          }
+            hierarchy_id: req.body.hierarchy_id,
+          },
         }
       );
-      console.log('touched /hierarchy with PUT');
-      res.send('Successfully updated');
+      console.log("touched /hierarchy with PUT");
+      res.send("Successfully updated");
     } catch (err) {
       console.error(err);
-      res.send('Server error');
+      res.send("Server error");
     }
   })
 
   .post(async (req, res) => {
     const hierarchy = await db.sequelizeDB.query(hierarchySQL, {
-      type: sequelize.QueryTypes.SELECT
+      type: sequelize.QueryTypes.SELECT,
     });
     const currentId = (await hierarchy.length) + 1;
     try {
       const newHierarchy = await db.hierarchy.create({
         hierarchy_id: currentId,
         class: req.body.class,
-        phylum: req.body.phylum
+        phylum: req.body.phylum,
       });
 
-      console.log('touched /hierarchy with POST');
+      console.log("touched /hierarchy with POST");
       res.json(newHierarchy);
     } catch (err) {
       console.error(err);
-      res.send('Server error');
+      res.send("Server error");
     }
   })
 
@@ -72,14 +76,14 @@ router.route('/hierarchy')
     try {
       await db.hierarchy.destroy({
         where: {
-          hierarchy_id: req.params.hierarchy_id
-        }
+          hierarchy_id: req.params.hierarchy_id,
+        },
       });
-      console.log('touched /hierarchy with DELETE');
-      res.send('Successfully deleted');
+      console.log("touched /hierarchy with DELETE");
+      res.send("Successfully deleted");
     } catch (err) {
       console.error(err);
-      res.send('Server error');
+      res.send("Server error");
     }
   });
 
